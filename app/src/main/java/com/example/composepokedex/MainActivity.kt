9e4890d3920f334c1c37a0bd2d3adc82d4a146c1
Amarvenkat <amarvenkat30@gmail.com>
2023-07-07 22:30:45 +0530

    Network Call

--- app/src/main/java/com/example/composepokedex/MainActivity.kt
+++ app/src/main/java/com/example/composepokedex/MainActivity.kt
@@ -15,8 +15,8 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.navArgument
 import androidx.navigation.compose.rememberNavController
-import androidx.navigation.navArgument
 import com.example.composepokedex.ui.theme.ComposePokedexTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -30,6 +30,7 @@ class MainActivity : ComponentActivity() {
                 val navController = rememberNavController()
                 NavHost(navController = navController, startDestination = "pokemon_list_screen" ){
                     composable("pokemon_list_screen"){
+
 
                     }
                     composable("pokemon_detail_screen/{dominantColor}/{pokemonName}",
